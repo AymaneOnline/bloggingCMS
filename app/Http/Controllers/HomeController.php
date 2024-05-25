f326d1c0fe1fbe10bb953423f6dfafd506b1e471
AymaneOnline <138299321+AymaneOnline@users.noreply.github.com>
2024-05-25 11:02:37 +0100

    Add comment on HomeController

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -4,6 +4,8 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 
+// I'm adding this comment to test git commands (status, add, commit)
+
 class HomeController extends Controller
 {
     public function __invoke()
