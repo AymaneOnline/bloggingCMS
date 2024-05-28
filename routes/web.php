35c4b4b14906236945883313e3ec6311eff33fab
AymaneOnline <138299321+AymaneOnline@users.noreply.github.com>
2024-05-28 07:01:18 +0100

    Update

--- routes/web.php
+++ routes/web.php
@@ -1,10 +1,14 @@
 <?php
 
+use App\Http\Controllers\FallbackController;
 use App\Http\Controllers\HomeController;
 use App\Http\Controllers\PostsController;
 use Illuminate\Support\Facades\Route;
 
-Route::resource('/blog', PostsController::class);
+Route::resource('blog', PostsController::class);
 
 // Route for invoke method
 Route::get('/', HomeController::class);
+
+// Fallback route
+Route::fallback(FallbackController::class);
